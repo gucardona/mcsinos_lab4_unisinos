e23b3a69d5a131f715096b5fda53b976c7e4cc9b
Gustavo Parcianello Cardona <gupcardona@gmail.com>
2023-06-07 09:08:23 -0300

    Adicao de tests

--- src/tests/OperacoesTest.java
+++ src/tests/OperacoesTest.java
@@ -1,10 +1,37 @@
 package tests;
 
+import operacoes.Operacoes;
+import org.junit.Before;
 import org.junit.Test;
+import pedido.Cardapio;
+import pedido.FilaDePedidos;
+import pedido.Pedido;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class OperacoesTest {
-    @Test
-    public void testa_operacoes() {
+    FilaDePedidos filaDePedidos = new FilaDePedidos();
+    ArrayList<Cardapio> itens = new ArrayList<>();
+    Pedido pedido = new Pedido("Carlos", itens, (Cardapio.BROWNIE.getPreco() + Cardapio.CAPUCCINO.getPreco()), LocalDateTime.now());
 
+    @Before
+    public void adicionaItens() {
+        itens.add(Cardapio.BROWNIE);
+        itens.add(Cardapio.CAPUCCINO);
+        filaDePedidos.adicionarPedido(pedido);
+    }
+
+    @Test
+    public void testa_adicao() {
+        assertEquals(filaDePedidos.getFilaDePedidos().peek(), pedido);
+    }
+
+    @Test
+    public void testa_entrega() {
+        filaDePedidos.entregarPedido();
+        assertEquals(filaDePedidos.getFilaDePedidos().peek(), null);
     }
 }
